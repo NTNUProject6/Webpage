64bb75d7cc27f78d57d20d5477ba38feb894e829
Einar Hov <einhov@gmail.com>
2014-10-08 19:33:17 +0200

    Add servlet for admin session login form
    Also add a test page that checks for admin status.

---
+++ src/no/ntnu/cabinet/AdminLogin.java
@@ -0,0 +1,47 @@
+package no.ntnu.cabinet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AdminLogin
+ */
+@WebServlet("/AdminLogin")
+public class AdminLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		String adminPassword = "CabiNet";
+		String password = request.getParameter("formPassword");
+		if(!password.equals(adminPassword)) {
+			out.println("<b>Incorrect password.</b>");
+			out.println(password);
+		}
+		else {
+			out.println("Correct password.");
+			HttpSession session = request.getSession();
+			session.setAttribute("isAdmin", true);
+		}
+	}
+
+}
