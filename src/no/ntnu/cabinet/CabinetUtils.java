5a7bb318f8e6e06289be883312ba2af285134f9a
Einar Hov <einhov@gmail.com>
2014-10-28 22:12:49 +0100

    Add CabinUtil class with utility functions
    Currently only a booking collision checker.

---
+++ src/no/ntnu/cabinet/CabinetUtils.java
@@ -0,0 +1,21 @@
+package no.ntnu.cabinet;
+
+import java.util.Date;
+import java.util.ArrayList;
+import no.ntnu.cabinet.database.Database;
+import no.ntnu.cabinet.database.Booking;
+
+public class CabinetUtils {
+	public static boolean BookingCollides(int cabin_id, Date from_date, Date to_date) {
+		Database db = new Database();
+		ArrayList<Booking> bookings = db.getBooking(cabin_id);
+		db.close();
+		for(Booking b : bookings) {
+			if(to_date.compareTo(b.getDate_To()) <= 0 && to_date.compareTo(b.getDate_From()) >= 0)
+				return true;
+			else if(from_date.compareTo(b.getDate_To()) <= 0 && from_date.compareTo(b.getDate_From()) >= 0)
+				return true;
+		}
+		return false;
+	}
+}
