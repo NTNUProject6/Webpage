ab22aa07fc8a3be3a3322e703770089fc8810a18
Einar Hov <einhov@gmail.com>
2014-11-17 14:15:56 +0100

    Properly close ResultsSets and statements

--- src/no/ntnu/cabinet/database/Database.java
+++ src/no/ntnu/cabinet/database/Database.java
@@ -14,8 +14,8 @@ public class Database {
 	
 	private static String url = "jdbc:mysql://cabinet-ntnu.no-ip.org/cabinet",
 					username = "cabinet", password = "CabiNet";
-			
-	public Database(){
+
+	public Database() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (Exception e) {
@@ -23,12 +23,10 @@ public class Database {
 		}
 		try {
 			connection = DriverManager.getConnection(url, username, password);
-			
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public void close() {
 		if(connection != null) {
 			try {
@@ -38,53 +36,59 @@ public class Database {
 			}
 		}
 	}
-	
-	public int addBooking(Booking booking){
+
+	public int addBooking(Booking booking) {
+		int id;
+		PreparedStatement pStatement = null;
+		ResultSet rs = null;
 		try{
 			statement = connection.createStatement();
 			String query = "INSERT INTO bookings (user_id, cabin_id, date_from, date_to) "
 					+ "VALUES (?, ?, ?, ?)";
-			PreparedStatement pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); 
+			pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); 
 			pStatement.setString(1, booking.getUser_id());
 			pStatement.setInt(2, booking.getCabin_id());
 			pStatement.setDate(3, new java.sql.Date(booking.getDate_From().getTime()));
 			pStatement.setDate(4, new java.sql.Date(booking.getDate_To().getTime()));
-			
+
 			pStatement.execute();
-			ResultSet rs = pStatement.getGeneratedKeys();
-			statement.close();
-			if(rs.next()) {
-				int id = rs.getInt(1);
-				rs.close();
-				return id;
-			} else {
-				rs.close();
-				return -2;
-			}
-		} catch (Exception e){
+			rs = pStatement.getGeneratedKeys();
+
+			if(rs.next())
+				id = rs.getInt(1);
+			else
+				id = -2;
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			return -1;
+			id = -1;
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
+		return id;
 	}
-	
+
 	public void deleteBooking(int id) {
 		try{
 		statement = connection.createStatement();
 		String query = "DELETE FROM bookings WHERE booking_id=" + id;
 		statement.execute(query);
-		statement.close();
 		} catch(Exception e) {
-			return;
+			e.printStackTrace();
+		} finally {
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
 	}
 	
-	public Booking getBookingById(int booking_id){
+	public Booking getBookingById(int booking_id) {
 		Booking b = new Booking();
+		ResultSet rs = null;
 		
 		try{
 			statement = connection.createStatement();
 			String sql = "select * from bookings where booking_id = " + booking_id;
-			ResultSet rs = statement.executeQuery(sql);
+			rs = statement.executeQuery(sql);
 			if(rs.next()) {
 				b.setId(rs.getInt("booking_id"));
 				b.setUser_id(rs.getString("user_id"));
@@ -92,20 +96,24 @@ public class Database {
 				b.setDate_From(rs.getDate("date_from"));
 				b.setDate_To(rs.getDate("date_to"));
 			}
-		} catch(SQLException se){
+		} catch(SQLException se) {
 			se.printStackTrace();
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
 		
 		return b;
 		
 	}
 	
-	public ArrayList<Booking> getBooking(int cabin_id){
+	public ArrayList<Booking> getBooking(int cabin_id) {
 		ArrayList<Booking> ab = new ArrayList<Booking>();
+		ResultSet rs = null;
 		try{
 			statement = connection.createStatement();
 			String sql = "select * from bookings where cabin_id = " + cabin_id + " order by date_from asc";
-			ResultSet rs = statement.executeQuery(sql);
+			rs = statement.executeQuery(sql);
 			while(rs.next()){
 				Booking b = new Booking();
 				b.setId(rs.getInt("booking_id"));
@@ -115,20 +123,26 @@ public class Database {
 				b.setDate_To(rs.getDate("date_to"));
 				ab.add(b);
 			}
-			rs.close();
-			statement.close();
-		} catch(SQLException se){
+		} catch(SQLException se) {
 			se.printStackTrace();
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
+
 		return ab;
 	}
 	
 	public int addReport(Report report) {
+		PreparedStatement pStatement = null;
+		ResultSet rs = null;
+		int id = 0;
+
 		try{
 			statement = connection.createStatement();
 			String query = "INSERT INTO reports (cabin_id, wood, damage, missing, other, email, report_date) "
 					+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); 
+			pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); 
 			pStatement.setInt(1, report.getCabin_id());
 			pStatement.setInt(2, report.getWood());
 			pStatement.setString(3, report.getDamage());
@@ -138,28 +152,31 @@ public class Database {
 			pStatement.setDate(7, new java.sql.Date(report.getReport_date().getTime()));
 			pStatement.execute();
 			
-			ResultSet rs = pStatement.getGeneratedKeys();
-			statement.close();
+			rs = pStatement.getGeneratedKeys();
 			if(rs.next()) {
-				int id = rs.getInt(1);
-				rs.close();
-				return id;
+				id = rs.getInt(1);
 			} else {
-				rs.close();
-				return -2;
+				id = -2;
 			}
 		} catch (Exception e){
 			e.printStackTrace();
+			id = -1;
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
-		return -1;
+
+		return id;
 	}
 	
-	public ArrayList<Report> getReports(int cabin_id){
+	public ArrayList<Report> getReports(int cabin_id) {
 		ArrayList<Report> rp = new ArrayList<Report>();
+		ResultSet rs = null;
+
 		try{
 			statement = connection.createStatement();
 			String sql = "select * from reports where cabin_id = " + cabin_id + " order by report_date desc";
-			ResultSet rs = statement.executeQuery(sql);
+			rs = statement.executeQuery(sql);
 			while(rs.next()){
 				Report r = new Report();
 				r.setWood(rs.getInt("wood"));
@@ -170,20 +187,24 @@ public class Database {
 				r.setOther(rs.getString("other"));
 				rp.add(r);
 			}
-			rs.close();
-			statement.close();
-		} catch(SQLException se){
+		} catch(SQLException se) {
 			se.printStackTrace();
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
+
 		return rp;
 	}
 	
 	public Report getReportById(int report_id) {
 		Report r = new Report();
+		ResultSet rs = null;
+
 		try {
 			statement = connection.createStatement();
 			String sql = "select * from reports where id = " + report_id;
-			ResultSet rs = statement.executeQuery(sql);
+			rs = statement.executeQuery(sql);
 			if(rs.next()) {
 				r.setCabin_id(rs.getInt("cabin_id"));
 				r.setWood(rs.getInt("wood"));
@@ -193,19 +214,24 @@ public class Database {
 				r.setEmail(rs.getString("email"));
 				r.setOther(rs.getString("other"));
 			}
-		} catch(SQLException se){
+		} catch(SQLException se) {
 			se.printStackTrace();
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
-		
+
 		return r;
 	}
 	
-	public Cabin getCabin(int cabin_id){
+	public Cabin getCabin(int cabin_id) {
 		Cabin cabin = new Cabin();
+		ResultSet rs = null;
+
 		try{
 			statement = connection.createStatement();
 			String sql = "select * from cabins where cabin_id = " + cabin_id + ";";
-			ResultSet rs = statement.executeQuery(sql);
+			rs = statement.executeQuery(sql);
 			while(rs.next()){
 				cabin.setName(rs.getString("cabin_name"));
 				cabin.setWood(rs.getInt("cabin_wood"));
@@ -216,29 +242,36 @@ public class Database {
 				cabin.setCapacity(rs.getInt("beds"));
 				cabin.setDifficulty(rs.getInt("difficulty"));
 			}
-			rs.close();
-			statement.close();
-		} catch(SQLException se){
+		} catch(SQLException se) {
 			se.printStackTrace();
+		} finally {
+			try{ if(rs != null) rs.close(); } catch(Exception e) {};
+			try{ if(statement != null) statement.close(); } catch(Exception e) {};
 		}
+
 		cabin.setId(cabin_id);
 		return cabin;
 	}
 	
 	public boolean updateWood(Report r) {
 		Cabin c = getCabin(r.getCabin_id());
+
 		if(c.getWood_updated().compareTo(r.getReport_date()) <= 0) {
+			PreparedStatement pStatement = null;
+
 			try{
 				statement = connection.createStatement();
 				String query = "UPDATE cabins SET cabin_wood=?,wood_updated=? WHERE cabin_id=" + c.getId();
-				PreparedStatement pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+				pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 				pStatement.setInt(1, r.getWood());
 				pStatement.setDate(2, new java.sql.Date(r.getReport_date().getTime()));
 				pStatement.execute();
-				statement.close();
 			} catch(SQLException se) {
 				se.printStackTrace();
+			} finally {
+				try{ if(statement != null) statement.close(); } catch(Exception e) {};
 			}
+
 			return true;
 		}
 		return false;
